e1a2ac479f0f4d22f99ece60697964b924688b72
Clive Crous <clive@crous.co.za>
2006-11-23 22:04:03 +0000

    OpenGL initialisation and finalise tutorial 1 (for now)

--- lesson_01/lesson_01.rb
+++ lesson_01/lesson_01.rb
@@ -2,13 +2,35 @@
 # A basic environment. This simply waits for you to close the window, then
 # exits.
 require 'rubygame'
+require 'opengl'
+
+WIDTH = 400
+HEIGHT = 300
 
 Rubygame.init
 
-screen = Rubygame::Screen.set_mode [400,300], 32,
+# Setup screen
+
+screen = Rubygame::Screen.set_mode [WIDTH,HEIGHT], 32,
   Rubygame::HWSURFACE|Rubygame::OPENGL
 
 screen.set_caption "neheruby - Lesson 1"
+
+# Setup initial OpenGL
+
+GL::Viewport 0, 0, WIDTH, HEIGHT
+
+GL::MatrixMode GL::PROJECTION
+GL::LoadIdentity()
+GLU::Perspective 45, WIDTH/(HEIGHT.to_f), 3, 10
+
+GL::MatrixMode GL::MODELVIEW
+GL::LoadIdentity()
+
+GL::Enable GL::DEPTH_TEST
+GL::DepthFunc GL::LESS
+
+# Main loop
 
 queue = Rubygame::Queue.instance
 loop do
@@ -18,6 +40,12 @@ loop do
         exit
     end
   end
-  screen.update
+
+  GL::Clear GL::COLOR_BUFFER_BIT|GL::DEPTH_BUFFER_BIT
+  GL::LoadIdentity()
+
+  # Draw scene here
+
+  Rubygame::GL.swap_buffers
 end
 
