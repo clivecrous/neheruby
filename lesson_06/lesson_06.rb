9832b2960110db9425f641269ce3686f46051454
Clive Crous <clive@crous.co.za>
2006-11-24 20:01:53 +0000

    initial lesson 6 code

--- lesson_06/lesson_06.rb
+++ lesson_06/lesson_06.rb
@@ -4,105 +4,114 @@ require 'opengl'
 
 require 'basecode/nehe'
 
-class Lesson05 < Display
+class Lesson06 < Display
 
   def initialize width, height
-    super width, height, "Lesson 5"
+    super width, height, "Lesson 6"
 
-    @angle_triangle = 0
-    @angle_quads = 0
+    @xrot = 0
+    @yrot = 0
+    @zrot = 0
   end
 
-  def render_pyramid
-    GL::Begin GL::TRIANGLES
-          GL::Color 1,0,0
-          GL::Vertex  0, 1, 0
-          GL::Color 0,1,0
-          GL::Vertex -1,-1, 1
-          GL::Color 0,0,1
-          GL::Vertex  1,-1, 1
+  def gl_setup
+    super
 
-          GL::Color 1,0,0
-          GL::Vertex  0, 1, 0
-          GL::Color 0,0,1
-          GL::Vertex  1,-1, 1
-          GL::Color 0,1,0
-          GL::Vertex  1,-1,-1
+    loadGLtextures
 
-          GL::Color 1,0,0
-          GL::Vertex  0, 1, 0
-          GL::Color 0,1,0
-          GL::Vertex  1,-1,-1
-          GL::Color 0,0,1
-          GL::Vertex -1,-1,-1
+    GL::Enable GL::TEXTURE_2D
+    GL::ShadeModel GL::SMOOTH
 
-          GL::Color 1,0,0
-          GL::Vertex  0, 1, 0
-          GL::Color 0,0,1
-          GL::Vertex -1,-1,-1
-          GL::Color 0,1,0
-          GL::Vertex -1,-1, 1
-    GL::End()
+  end
+
+  def loadGLtextures
+    texture = Rubygame::Image.load("data/nehe.bmp")
+
+    @textures = GL::GenTextures 1
+
+    GL::BindTexture GL::TEXTURE_2D, @textures[0]
+
+    GL::TexImage2D GL::TEXTURE_2D, 0, GL::RGB, texture.w, texture.h, 0, GL::BGR,
+                   GL::UNSIGNED_BYTE, texture.pixels
+
+    GL::TexParameter GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER,GL::LINEAR
+    GL::TexParameter GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER,GL::LINEAR
   end
 
   def render_cube
+    GL::BindTexture GL::TEXTURE_2D, @textures[0]
+
     GL::Begin GL::QUADS
-          GL::Color 0,1,0
-          GL::Vertex  1, 1,-1
-          GL::Vertex -1, 1,-1
-          GL::Vertex -1, 1, 1
-          GL::Vertex  1, 1, 1
-
-          GL::Color 1,0.5,0
-          GL::Vertex  1,-1,-1
-          GL::Vertex -1,-1,-1
+          GL::TexCoord 0, 1
           GL::Vertex -1,-1, 1
+          GL::TexCoord 1, 1
           GL::Vertex  1,-1, 1
-
-          GL::Color 1,0,0
+          GL::TexCoord 1, 0
           GL::Vertex  1, 1, 1
+          GL::TexCoord 0, 0
           GL::Vertex -1, 1, 1
-          GL::Vertex -1,-1, 1
-          GL::Vertex  1,-1, 1
 
-          GL::Color 1,1,0
-          GL::Vertex  1, 1,-1
-          GL::Vertex -1, 1,-1
+          GL::TexCoord 0, 0
           GL::Vertex -1,-1,-1
+          GL::TexCoord 0, 1
+          GL::Vertex -1, 1,-1
+          GL::TexCoord 1, 1
+          GL::Vertex  1, 1,-1
+          GL::TexCoord 1, 0
           GL::Vertex  1,-1,-1
 
-          GL::Color 0,0,1
-          GL::Vertex -1, 1, 1
+          GL::TexCoord 1, 1
           GL::Vertex -1, 1,-1
+          GL::TexCoord 1, 0
+          GL::Vertex -1, 1, 1
+          GL::TexCoord 0, 0
+          GL::Vertex  1, 1, 1
+          GL::TexCoord 0, 1
+          GL::Vertex  1, 1,-1
+
+          GL::TexCoord 0, 1
           GL::Vertex -1,-1,-1
+          GL::TexCoord 1, 1
+          GL::Vertex  1,-1,-1
+          GL::TexCoord 1, 0
+          GL::Vertex  1,-1, 1
+          GL::TexCoord 0, 0
           GL::Vertex -1,-1, 1
 
-          GL::Color 1,0,1
-          GL::Vertex  1, 1, 1
-          GL::Vertex  1, 1,-1
+          GL::TexCoord 0, 0
           GL::Vertex  1,-1,-1
+          GL::TexCoord 0, 1
+          GL::Vertex  1, 1,-1
+          GL::TexCoord 1, 1
+          GL::Vertex  1, 1, 1
+          GL::TexCoord 1, 0
           GL::Vertex  1,-1, 1
+
+          GL::TexCoord 1, 0
+          GL::Vertex -1,-1,-1
+          GL::TexCoord 0, 0
+          GL::Vertex -1,-1, 1
+          GL::TexCoord 0, 1
+          GL::Vertex -1, 1, 1
+          GL::TexCoord 1, 1
+          GL::Vertex -1, 1,-1
     GL::End()
   end
 
   def render_objects
-    GL::Translate -1.5, 0, -6
+    GL::Translate 0, 0, -6
 
-    GL::Rotate @angle_triangle,0,1,0
-
-    render_pyramid
-
-    GL::LoadIdentity()
-    GL::Translate 1.5,0,-6
-
-    GL::Rotate @angle_quads,1,0,0
+    GL::Rotate @xrot, 1, 0, 0
+    GL::Rotate @yrot, 0, 1, 0
+    GL::Rotate @zrot, 0, 0, 1
 
     render_cube
 
-    @angle_triangle += 0.2
-    @angle_quads += 0.15
+    @xrot += 0.3
+    @yrot += 0.2
+    @zrot += 0.4
   end
 
 end
 
-Lesson05.new( 400, 300 ).run
+Lesson06.new( 400, 300 ).run
