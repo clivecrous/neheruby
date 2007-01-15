30bd27361544b75231ec71a28e55cea0b073663b
Clive Crous <clive@crous.co.za>
2007-01-15 07:13:30 +0000

    Add texture types as filtering

--- lesson_07/lesson_07.rb
+++ lesson_07/lesson_07.rb
@@ -15,9 +15,9 @@ class Lesson07 < Display
     @yrot_delta = 0
     @zoom=5.0
 
-    @light = false
+    @filter = 0
 
-    @lighting = {}
+    @light = false
   end
 
   def gl_setup
@@ -39,19 +39,36 @@ class Lesson07 < Display
   def loadGLtextures
     texture = Rubygame::Image.load("data/crate.bmp")
 
-    @textures = GL::GenTextures 1
+    @textures = GL::GenTextures 3
 
     GL::BindTexture GL::TEXTURE_2D, @textures[0]
+
+    GL::TexParameter GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER,GL::NEAREST
+    GL::TexParameter GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER,GL::NEAREST
 
     GL::TexImage2D GL::TEXTURE_2D, 0, GL::RGB, texture.w, texture.h, 0, GL::BGR,
                    GL::UNSIGNED_BYTE, texture.pixels
 
+    GL::BindTexture GL::TEXTURE_2D, @textures[1]
+
     GL::TexParameter GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER,GL::LINEAR
     GL::TexParameter GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER,GL::LINEAR
+
+    GL::TexImage2D GL::TEXTURE_2D, 0, GL::RGB, texture.w, texture.h, 0, GL::BGR,
+                   GL::UNSIGNED_BYTE, texture.pixels
+
+    GL::BindTexture GL::TEXTURE_2D, @textures[2]
+
+    GL::TexParameter GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER,
+      GL::LINEAR_MIPMAP_NEAREST
+    GL::TexParameter GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER,GL::LINEAR
+
+    GLU::Build2DMipmaps GL::TEXTURE_2D, GL::RGB, texture.w, texture.h, GL::BGR,
+                   GL::UNSIGNED_BYTE, texture.pixels
   end
 
   def render_cube
-    GL::BindTexture GL::TEXTURE_2D, @textures[0]
+    GL::BindTexture GL::TEXTURE_2D, @textures[ @filter ]
 
     GL::Begin GL::QUADS
           GL::Normal 0,0,1
@@ -153,6 +170,8 @@ class Lesson07 < Display
             else
               GL::Disable GL::LIGHTING
             end
+          when Rubygame::K_F
+            @filter = ( @filter + 1) % 3
         end
     end
   end
